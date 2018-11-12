70cf3786b357df903776239287bc0663c5b7151b
Sangwon Hong <qpakzk@gmail.com>
2018-11-12 15:40:59 +0900

    p6: initial sourcecode of project 6

---
+++ ssuos_p6/src/kernel/filesys/procfs.c
@@ -0,0 +1,67 @@
+#include <filesys/procfs.h>
+#include <filesys/vnode.h>
+#include <proc/proc.h>
+#include <list.h>
+#include <string.h>
+#include <ssulib.h>
+
+extern struct list p_list;
+extern struct process *cur_process;
+
+struct vnode *init_procfs(struct vnode *mnt_vnode)
+{
+	mnt_vnode->v_op.ls = proc_process_ls;
+	mnt_vnode->v_op.mkdir = NULL;
+	mnt_vnode->v_op.cd = proc_process_cd;
+
+	return mnt_vnode;
+}
+
+int proc_process_ls()
+{
+	int result = 0;
+	struct list_elem *e;
+
+	printk(". .. ");
+	for(e = list_begin (&p_list); e != list_end (&p_list); e = list_next (e))
+	{
+		struct process* p = list_entry(e, struct process, elem_all);
+
+		printk("%d ", p->pid);
+	}
+	printk("\n");
+
+	return result;
+}
+
+int proc_process_cd(char *dirname)
+{
+
+}
+
+int proc_process_info_ls()
+{
+	int result = 0;
+
+	printk(". .. ");
+	printk("cwd root time stack");
+
+	printk("\n");
+
+	return result;
+}
+
+int proc_process_info_cd(char *dirname)
+{
+
+}
+
+int proc_process_info_cat(char *filename)
+{
+
+}
+
+int proc_link_ls()
+{
+
+}
